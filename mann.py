1ff5e17fb42679dd9b44812b0038c295809cd498
assetgit-adobe <122297181+assetgit-adobe@users.noreply.github.com>
2023-09-12 13:23:58 -0700

    Update mann.py

--- mann.py
+++ mann.py
@@ -1,8 +1,5 @@
 import sys
 token = 'SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+def osd():
+  sys.create_change()
